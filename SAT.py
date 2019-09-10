b52f6ce4f833e0691c3b4807549262291220cd7f
Valerie <vsawirja3@gmail.com>
2019-09-10 20:59:43 +0200

    created SAT class + get clauses and var from inputfile methods

--- SAT.py
+++ SAT.py
@@ -1,6 +1,67 @@
 import sys
-import os.path
+import pathlib
 
+class SAT(object):
+    """Representation of a SAT solver"""
+
+    def __init__(self, clauses_file):
+        """
+        Initializes clauses and start values of SAT solver
+        """
+        # values should be a dict, only absolute values
+        self.values = {}
+        self.clauses = self.load_clauses(clauses_file)
+
+    def load_clauses(self, clauses_file):
+        """
+        reads in the clauses from a file
+        returns a list/set --> list of lists
+        returns a dict with all the variables and their truth assignment
+        """
+        # https://stackoverflow.com/questions/28890268/parse-dimacs-cnf-file-python
+        filepath = pathlib.Path(clauses_file)
+
+        # create a list in a list
+        clauses = list()
+        clauses.append(list())
+
+        # read in the file
+        with filepath.open('r') as f:
+            for line in f:
+                symbols = line.split()
+
+                # skip empty lines and lines with comments
+                if len(symbols) != 0 and symbols[0] not in ("p", "c"):
+                    for symbol in symbols:
+                        literal = int(symbol)
+
+                        # add new list if line is terminated with 0
+                        if literal == 0:
+                            clauses.append(list())
+                        else:
+                            # add literal to the last list in list of lists
+                            clauses[-1].append(literal)
+                            # add literal to self.values
+                            self.init_values(literal)
+        return clauses
+
+
+    def init_values(self, literal):
+        """
+        checks if the literal is already in the self.values dict,
+        if it's not, it creates a new key-value pair (only absolute values)
+        ex. 'literal': '?'
+        """
+        if abs(literal) not in self.values:
+            self.values[literal] = '?'
+
+    def davis_putnam(self):
+        """
+        runs the davis putnam algorithm.
+        recursively???
+        :return: an DIMACS output file with the true variables
+        """
+        # extra var with cleared clauses (pop from old and append to cleared list)
 
 if __name__ == "__main__":
 
@@ -8,7 +69,7 @@ if __name__ == "__main__":
 
     # ensure correct usage
     if len(sys.argv) != 3:
-        print("usage: SAT.py -Sn clause_file+start_values_file")
+        print("usage: SAT.py -Sn inputfile.txt")
         exit(1)
 
     # ensure the strategy number is valid
@@ -17,19 +78,24 @@ if __name__ == "__main__":
         print("usage: pick 1, 2, or 3 as strategy number, ex. -S1")
         exit(1)
 
-    # parse the concatenation and ensure the files exist
-    files = sys.argv[2].split("+")
 
-    for file in files:
-        if not os.path.isfile(file):
-            # does the below also filter out empty files?
-            print("{} does not exist".format(file))
-            exit(1)
+    inputfile = sys.argv[2]
+    # add extension if absent
+    if ".txt" not in inputfile:
+        inputfile = inputfile + ".txt"
+    # ensure the file exist
+    path = pathlib.Path(inputfile)
+    if not path.exists():
+        print("{} does not exist".format(inputfile))
+        exit(1)
 
-    clauses_file = files[0]
-    start_values_file = files[1]
+    # create a SAT object and read in the files
+    solver = SAT(inputfile)
+
+    # test lineeeesss
+    print(solver.clauses)
+    print(solver.values)
 
     print("mlep")
 
-    # create a sat object and read in the files
 
